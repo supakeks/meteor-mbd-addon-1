e21471e96e50f0a359791b6905dbb113ee113f86
seasnail8169 <seasnail8169@protonmail.com>
2021-03-22 14:51:48 +0000

    Fix crash

--- src/main/java/meteordevelopment/addons/mountbypassdupe/MountBypassDupeAddon.java
+++ src/main/java/meteordevelopment/addons/mountbypassdupe/MountBypassDupeAddon.java
@@ -2,11 +2,7 @@ package meteordevelopment.addons.mountbypassdupe;
 
 import minegame159.meteorclient.MeteorAddon;
 import minegame159.meteorclient.MeteorClient;
-import minegame159.meteorclient.modules.Category;
 import minegame159.meteorclient.modules.Modules;
-import minegame159.meteorclient.modules.world.AutoMountBypassDupe;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 
 public class MountBypassDupeAddon extends MeteorAddon {
 
